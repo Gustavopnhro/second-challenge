0b9d06b4b5f8e354607f85ef63f3a874cceaa850
Gustavo <131812094+Gustavopnhro@users.noreply.github.com>
2023-10-18 07:43:38 -0300

    Update TaskService.java

--- src/main/java/dio/me/secondchallenge/service/TaskService.java
+++ src/main/java/dio/me/secondchallenge/service/TaskService.java
@@ -1,13 +1,10 @@
 package dio.me.secondchallenge.service;
 
-import org.springframework.stereotype.Service;
-
 import dio.me.secondchallenge.domain.model.Task;
 
-@Service
 public interface TaskService {
     
-    Task findById(Integer id);
+    Task findById(Long id);
 
     Task create(Task taskToCreate);
 }
